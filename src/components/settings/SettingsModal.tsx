6b46d750d8aa690dc19915a97d126b96db1da8b9
Tin <jaytintran@gmail.com>
2026-08-26 14:31:58 +0700

    add more options for books per row

--- src/components/settings/SettingsModal.tsx
+++ src/components/settings/SettingsModal.tsx
@@ -351,6 +351,9 @@ export function SettingsModal() {
                       { value: 3, label: "3" },
                       { value: 4, label: "4" },
                       { value: 5, label: "5" },
+                      { value: 6, label: "6" },
+                      { value: 7, label: "7" },
+                      { value: 8, label: "8" },
                     ]}
                   />
                 </div>
